241361c68a1789a626bded41d09be66331083d12
Chamuditha Kalhara <chamudithakalhara123@gmail.com>
2025-05-26 17:53:55 +0530

    Update

--- app/src/main/java/com/example/limitly/MainActivity.java
+++ app/src/main/java/com/example/limitly/MainActivity.java
@@ -7,9 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,36 +19,6 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        //find & assign navbar id to bottomNavigationView
-        BottomNavigationView bottomNavigationView = findViewById(R.id.navbar);
-
-        //display default page as HomeFragment
-        getSupportFragmentManager().beginTransaction().replace(R.id.main, new HomeFragment()).commit();
-
-        //mark the selected tab is home in navbar item
-        bottomNavigationView.setSelectedItemId(R.id.homeNav);
-
-        //handle page navigation
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            Fragment fragment = null;
-            int itemId = item.getItemId();
-
-            if (itemId == R.id.homeNav) {
-                fragment = new HomeFragment();
-            } else if (itemId == R.id.appsNav) {
-                fragment = new AppsFragment();
-            } else if (itemId == R.id.settingsNav) {
-                fragment = new SettingsFragment();
-            }
-
-            if (fragment != null) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.main, fragment).commit();
-                return true;
-            }
-
-            return false;
         });
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Limitly"
 include(":app")
- 
